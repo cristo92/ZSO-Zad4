6245cef0ee36e0170fc79c560585b96f49271b03
Krzysztof Leszczyński <kl319367@students.mimuw.edu.pl>
2014-08-21 14:24:34 +0200

    Dziala bardzo podstawowa wersja. Dodalem test1

--- Makefile
+++ Makefile
@@ -8,3 +8,6 @@ install:
 
 clean:
 	$(MAKE) -C $(KDIR) M=$$PWD clean
+
+test1: test1.c
+	gcc -o test1 test1.c
--- transdb.c
+++ transdb.c
@@ -28,51 +28,114 @@ MODULE_DESCRIPTION("Database");
 MODULE_LICENSE("GPL");
 
 #define MAX_SIZE 1024
+#define ACCEPT 1
+#define ROLLBACK 2
 
 int transdb_major;
-char db[MAX_SIZE];
+uint8_t db[MAX_SIZE];
 rwlock_t db_rwlock;
 char db_dirty;
 
 struct needle {
 	off_t pos;
 	uint8_t data;
+	struct list_head list;
 };
 
 struct transaction {
 	struct list_head list; //list of needles
+	struct semaphore sema;
 };
 
-char data[] = "siema\n";
-
-ssize_t transdb_read(struct file *filp, char __user *buff, size_t count, loff_t *offp) {
-	copy_to_user(data, buff, min(6, count));
-	return min(count, 6);
-}
 
 int transdb_open(struct inode *inode, struct file *file) {
 	struct transaction *trans = kmalloc(sizeof(struct transaction), GFP_KERNEL);
 	if(trans == 0) goto error;
 	INIT_LIST_HEAD(&trans->list);
+	sema_init(&trans->sema, 1);
 	file->private_data = trans;
 	printk(KERN_WARNING "Transdb open\n");
+	read_lock(&db_rwlock);
 	
 	return 0;
 error:
 	return -ENOMEM;
 }
 
+ssize_t transdb_read(struct file *filp, char __user *buff, size_t count, loff_t *offp) {
+	printk(KERN_WARNING "Transdb read\n");
+	if(copy_to_user(buff, db + *offp, count)) return -EFAULT;
+	*offp += count;
+	return count;
+}
+
+ssize_t transdb_write(struct file *filp, const char __user *buff, size_t count,
+		                loff_t *offp) {
+	struct transaction *trans = (struct transaction *)(filp->private_data);
+	struct needle *op;
+	int size = count;
+	printk(KERN_WARNING "Transdb write\n");
+	up(&trans->sema);
+	while(count) {
+		op = kmalloc(sizeof(struct needle), GFP_KERNEL); 
+		if(op == 0) goto out;
+		op->pos = *offp;
+		if(copy_from_user(&op->data, buff, 1)) {
+			kfree(op);
+			goto out;
+		}
+		list_add(&op->list, &trans->list);
+		count--;
+		(*offp)++;
+		buff++;
+	}
+out:
+	down(&trans->sema);
+	return size - count;
+}
+
+void finish_trans(struct transaction *trans, char flag) {
+	read_unlock(&db_rwlock);
+	down(&trans->sema);
+	if(!list_empty(&trans->list)) {
+		struct needle *pos;
+		struct needle *temp;
+		if(flag & ACCEPT) write_lock(&db_rwlock);
+		list_for_each_entry_safe(pos, temp, &trans->list, list) {
+			if(flag & ACCEPT) db[pos->pos] = pos->data;
+			list_del(&pos->list);
+			kfree(pos);
+		}
+		if(flag & ACCEPT) write_unlock(&db_rwlock);
+	}
+	up(&trans->sema);
+}
+
+void accept_trans(struct transaction *trans) {
+	finish_trans(trans, ACCEPT);
+}
+void rollback_trans(struct transaction *trans) {
+	finish_trans(trans, ROLLBACK);
+}
+
+int transdb_release(struct inode *inode, struct file *filp) {
+	struct transaction *trans = (struct transaction*)filp->private_data;
+	printk(KERN_WARNING "Transdb release\n");
+	accept_trans(trans);
+	return 0;
+}
+
 const static struct file_operations transdb_fops = {
 	owner: THIS_MODULE,
 	open: transdb_open,
-	read: transdb_read/*,
-	pread: transdb_pread,
+	read: transdb_read,
+	//pread: transdb_pread,
 	write: transdb_write,
-	pwrite: transdb_pwrite,
-	lseek: transdb_lseek,
+	//pwrite: transdb_pwrite,
+	//lseek: transdb_lseek,
 	release: transdb_release,
-	unlocked_ioctl: transdb_ioctl,
-compat_ioctl: transdb_ioctl*/
+	//unlocked_ioctl: transdb_ioctl,
+	//compat_ioctl: transdb_ioctl
 };
 
 int transdb_init_module(void) {
